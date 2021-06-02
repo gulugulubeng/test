c91318de2a9d86967db8e299576b8e86af34b212
gulugulubeng <2746160688@qq.com>
2021-06-03 01:04:57 +0800

    第一次使用第三方工具提交

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # test
 第一个git项目测试
 小号修改了readme文档！！！！！！！！！！！
+## 第一次使用第三方工具clone Github项目
--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test{
 	public static void mian(String[] args){
 		System.out.print("hello Git");
+		System.out.println("好久不见 java");
 	}
 }
